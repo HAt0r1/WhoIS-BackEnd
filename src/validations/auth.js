cdec11c3e70a7f8911c0ecd7f48b888afdc962a6
Hatori <ahgangadm@gmail.com>
2025-06-04 20:11:49 +0300

    Create Joi schema

---
+++ src/validations/auth.js
@@ -0,0 +1,15 @@
+import Joi from "joi";
+
+export const registrationValidation = Joi.object({
+    phone: Joi.string().required().pattern(/^\+?[0-9]{10,15}$/).messages({
+        'string.pattern.base': 'Phone number must be a valid format (e.g., +380123456789)',
+        'any.required': 'Phone number is required',
+    }),
+    password: Joi.string().required().min(6).messages({
+        'string.min': 'Password must be at least 6 characters long',
+        'any.required': 'Password is required',
+    }),
+    countryCode: Joi.string().optional().length(3).messages({
+            'string.length': 'Country code must be 3 characters long',
+    }),
+})
