2d788632ed98110c979f6a8c49de8d7ab8dd1374
Hatori <ahgangadm@gmail.com>
2025-06-05 21:52:17 +0300

    Add cookie parser

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import router from "./routers/index.js";
+import cookieParser from "cookie-parser";
 
 import {env} from "./utils/env.js";
 import {errorHandler} from "./middlewares/errorHandler.js";
@@ -12,14 +13,15 @@ const PORT = env('PORT');
 const setupServer = () => {
     const app = express();
 
+    app.use(cors());
+
     app.use(
         express.json({
             type: ['application/json', 'application/vnd.api+json'],
         }),
     );
 
-    app.use(cors());
-
+    app.use(cookieParser());
 
     app.use(
         pino({
