6f6b49ae15a2b4fd45518da16af82504ba112eb7
Hatori <ahgangadm@gmail.com>
2025-06-05 21:51:10 +0300

    Create session schema

---
+++ src/db/models/Session.js
@@ -0,0 +1,27 @@
+import {Schema, model} from "mongoose";
+
+const sessionSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    accessToken: {
+        type: String,
+        required: true
+    },
+    refreshToken: {
+        type: String,
+        required: true
+    },
+    accessTokenExpires: {
+        type: Date,
+        required: true
+    },
+    refreshTokenExpires: {
+        type: Date,
+        required: true
+    }
+}, {timestamps: true, versionKey: false});
+
+export default model("Session", sessionSchema);
