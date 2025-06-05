29eeebd4948e825aa5f42d4d094b09f753936caa
Hatori <ahgangadm@gmail.com>
2025-06-05 21:53:29 +0300

    Update register controller and add login controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,43 @@
-import {registration} from "../services/auth.js";
+import {registration, login} from "../services/auth.js";
+import {setupSession} from "../utils/sessionSetup.js";
+import {filterResUser} from "../utils/filterResUser.js";
 
-export const registrationUserController = async (req, res) => {
-    const user = await registration(req.body);
+export const registrationUserController = async (req, res, next) => {
 
-    res.status(200).json({
-        status: 200,
-        message: 'Successfully registered a user!',
-        data: {
-            phone: user.phone,
-        }
-    })
+    try {
+        const { user, refreshToken, sessionId, accessToken } = await registration(req.body);
+
+        const userWithoutTimestamps = filterResUser(user);
+
+        setupSession(res, sessionId, refreshToken);
+
+        res.status(200).json({
+            status: 200,
+            message: 'Successfully registered a user!',
+            data: {
+                user: userWithoutTimestamps,
+                accessToken,
+            }
+        });
+    } catch (error) {
+        next(error);
+    }
 }
+
+export const loginUserController = async (req, res) => {
+
+    const { user, session } = await login(req.body);
+    const userWithoutTimestamps = filterResUser(user);
+
+    setupSession(res, session._id, session.refreshToken);
+
+    res.json({
+        status: 200,
+        message: 'Successfully logged in a user!',
+        data: {
+            user: userWithoutTimestamps,
+            accessToken: session.accessToken,
+        },
+    });
+}
+
