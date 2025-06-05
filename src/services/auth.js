7ed7e23fe339aee2837c8c30fc42baece2d7f653
Hatori <ahgangadm@gmail.com>
2025-06-05 21:53:49 +0300

    Update register service and add login service

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,9 @@
 import createHttpError from "http-errors";
 import User from "../db/models/User.js";
+import Session from "../db/models/Session.js";
 import bcrypt from "bcrypt";
 
+import {createSession} from "../utils/sessionSetup.js";
 
 
 export const registration = async(payload) => {
@@ -12,8 +14,52 @@ export const registration = async(payload) => {
 
     const hashPass = await bcrypt.hash(payload.password, 10);
 
-    return User.create({
+    const newUser = await User.create({
        ...payload,
-        password: hashPass,
+       password: hashPass,
     });
+
+    const newSession = await createSession();
+
+    const session = await Session.create({
+        userId: newUser._id,
+        ...newSession,
+    }).catch(error => {
+        throw createHttpError(500, `Failed to create session ${error.message}`);
+    });
+
+    return {
+        user: newUser,
+        accessToken: session.accessToken,
+        refreshToken: session.refreshToken,
+        sessionId: session._id,
+    }
 };
+
+export const login = async(payload) => {
+    const user = await User.findOne({phone: payload.phone});
+    if (!user) {
+        throw createHttpError(404, 'User not found');
+    }
+
+    const isEqual = await bcrypt.compare(payload.password, user.password);
+
+    if (!isEqual) {
+        throw createHttpError(401, 'Invalid password');
+    }
+
+    await Session.deleteOne({userId: user._id});
+
+    const newSession = createSession();
+
+    const session = await Session.create({
+        userId: user._id,
+        ...newSession,
+    });
+
+    return { user, session };
+}
+
+export const logout = async(sessionId) => {
+    await Session.deleteOne({_id: sessionId});
+}
