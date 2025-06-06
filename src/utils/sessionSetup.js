828b0afec43321264ee185f04844f12715371f55
Hatori <ahgangadm@gmail.com>
2025-06-06 19:28:38 +0300

    Update session setup

--- src/utils/sessionSetup.js
+++ src/utils/sessionSetup.js
@@ -1,21 +1,18 @@
 import {randomBytes} from 'node:crypto';
-import {FORTY_MINUTES, SEVEN_DAYS} from "../constants/sessionExpires.js";
+import {SEVEN_DAYS} from "../constants/sessionExpires.js";
 
 export const createSession = () => {
+    // Генеруємо refreshToken
+    const refreshToken = randomBytes(30).toString('base64');  // Генерація refreshToken
 
-    const accessToken = randomBytes(30).toString('base64');
-    const refreshToken = randomBytes(30).toString('base64');
-    const accessTokenExpires = new Date(Date.now() + FORTY_MINUTES);
-    const refreshTokenExpires = new Date(Date.now() + SEVEN_DAYS);
-
+    // Встановлюємо термін дії refreshToken
+    const refreshTokenExpires = new Date(Date.now() + SEVEN_DAYS);  // Термін дії для refreshToken
 
     return {
-        accessToken,
         refreshToken,
-        accessTokenExpires,
         refreshTokenExpires,
     };
-}
+};
 
 export const setupSession = (res, sessionId, refreshToken) => {
     res.cookie('refreshToken', refreshToken, {
@@ -24,6 +21,7 @@ export const setupSession = (res, sessionId, refreshToken) => {
         sameSite: 'none',
         secure: true,
     });
+
     res.cookie('sessionId', sessionId, {
         httpOnly: true,
         expires: new Date(Date.now() + SEVEN_DAYS),
