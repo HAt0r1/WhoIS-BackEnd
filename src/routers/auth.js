84e4fa50df1a4d0494ca237f1352ddced8038430
Hatori <ahgangadm@gmail.com>
2025-06-06 19:29:51 +0300

    Create refresh route

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,7 +3,7 @@ import express from 'express';
 import {validateBody} from "../utils/validationBody.js";
 import {ctrlWrapper} from "../utils/ctrlWrapper.js";
 import {registrationValidation, loginValidation} from "../validations/auth.js";
-import {registrationUserController, loginUserController} from "../controllers/auth.js";
+import {registrationUserController, loginUserController, logoutUserController, refreshTokenController} from "../controllers/auth.js";
 
 const router = express.Router();
 
@@ -21,7 +21,12 @@ router.post(
 
 router.post(
     '/logout',
+    ctrlWrapper(logoutUserController),
+);
 
+router.post(
+    '/refresh',
+    ctrlWrapper(refreshTokenController),
 )
 
 export default router;
