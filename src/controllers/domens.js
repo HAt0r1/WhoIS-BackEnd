c8017a353b9f0450e11d2731cf4b8ed71b172cd1
Hatori <ahgangadm@gmail.com>
2025-06-10 22:03:02 +0300

    Update controller

--- src/controllers/domens.js
+++ src/controllers/domens.js
@@ -1,25 +1,36 @@
-import * as whoiser from 'whoiser';
+import axios from 'axios';
 import jwt from 'jsonwebtoken';
-
-
-import {env} from "../utils/env.js";
+import { env } from '../utils/env.js';
 
 export const searchDomain = async (req, res) => {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) return res.status(401).json({ message: 'No token' });
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) return res.status(401).json({ message: "No token" });
 
-    const { id: userId, role, countryCode: userCC } = jwt.verify(token, env('JWT_SECRET'));
+        const { role, countryCode: userCC } = jwt.verify(token, env("JWT_SECRET"));
 
-    const { domain } = req.body;
-    if (!domain) return res.status(400).json({ message: 'Domain required' });
+        const { domain } = req.body;
+        if (!domain) return res.status(400).json({ message: "Domain required" });
 
-    const whoisData = await whoiser(domain);
-    const domainCC = whoisData.country || whoisData['Registrant Country'] || whoisData['Registrant Country Code'];
-    if (!domainCC) return res.status(404).json({ message: 'Domain country code not found' });
+        const apiKey = env("WHOIS_API_KEY");
+        const apiUrl = `https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=${apiKey}&domainName=${domain}&outputFormat=JSON`;
 
-    if (role !== 'admin' && domainCC !== userCC) {
-        return res.status(403).json({ message: 'This domain is not available for your region.' });
+        const response = await axios.get(apiUrl);
+        const whoisData = response.data.WhoisRecord;
+
+        const domainCC = whoisData?.registrant?.countryCode;
+
+        if (!domainCC) {
+            return res.status(404).json({ message: "Domain country code not found" });
+        }
+
+        if (role !== "admin" && domainCC !== userCC) {
+            return res.status(403).json({ message: "This domain is not available for your region." });
+        }
+
+        res.json({ domain, whois: whoisData });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Something went wrong", data: err.message });
     }
-
-    res.json({ domain, whois: whoisData });
-}
+};
