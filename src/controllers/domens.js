1e397d392804dea740140b41ef7043d379f0f851
Hatori <ahgangadm@gmail.com>
2025-06-10 21:33:11 +0300

    Create controller for domens

---
+++ src/controllers/domens.js
@@ -0,0 +1,25 @@
+import whoiser from 'whoiser';
+import jwt from 'jsonwebtoken';
+
+
+import {env} from "../utils/env.js";
+
+export const searchDomain = async (req, res) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'No token' });
+
+    const { id: userId, role, countryCode: userCC } = jwt.verify(token, env('JWT_SECRET'));
+
+    const { domain } = req.body;
+    if (!domain) return res.status(400).json({ message: 'Domain required' });
+
+    const whoisData = await whoiser(domain);
+    const domainCC = whoisData.country || whoisData['Registrant Country'] || whoisData['Registrant Country Code'];
+    if (!domainCC) return res.status(404).json({ message: 'Domain country code not found' });
+
+    if (role !== 'admin' && domainCC !== userCC) {
+        return res.status(403).json({ message: 'Дані цього домену не доступні для вашого регіону' });
+    }
+
+    res.json({ domain, whois: whoisData });
+}
