d2205540e6af392810326d826986df8ec6f70746
Hatori <ahgangadm@gmail.com>
2025-06-11 19:03:46 +0300

    Update

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -6,7 +6,7 @@ const authenticate = (req, res, next) => {
         const authHeader = req.headers.authorization;
         if (!authHeader) return res.status(401).json({ message: 'Not authorized' });
         const token = authHeader.split(' ')[1];
-        req.user = jwt.verify(token, env('JWT_SECRET'));;
+        req.user = jwt.verify(token, env('JWT_SECRET'));
         next();
     } catch (err) {
         res.status(401).json({ message: 'Invalid or expired token' });
